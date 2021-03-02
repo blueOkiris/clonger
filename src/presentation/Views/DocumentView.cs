5d7d3a5cb00187de72a7d89686689ea087df814c
Dylan Turner <dylantdmt@gmail.com>
2021-03-02 12:54:59 -0500

    Created document view though it isn't working

--- src/presentation/Views/DocumentView.cs
+++ src/presentation/Views/DocumentView.cs
@@ -4,9 +4,39 @@ using Clonger.Data;
 
 namespace Clonger.Presentation.Views {
     class LanguageDocumentationView : VBox {
+        private TextView formatView;
+        
         public LanguageDocumentationView() :
-                base(false, (int) AppSettings.Margin) {
+                base(true, (int) AppSettings.Margin) {
+            addFormatView();
+            addInput();
+        }
+        
+        private void addFormatView() {
+            var formatFrame = new Frame("Format");
+            var formatScroll = new ScrolledWindow();
+            formatScroll.VscrollbarPolicy = PolicyType.Automatic;
+            formatScroll.HscrollbarPolicy = PolicyType.Never;
             
+            formatView = new TextView();
+            formatView.Editable = false;
+            
+            formatScroll.Add(formatView);
+            formatFrame.Add(formatScroll);
+            PackStart(formatFrame, true, true, AppSettings.Margin);
+        }
+        
+        private void addInput() {
+            var inputFrame = new Frame("Input");
+            var inputScroll = new ScrolledWindow();
+            inputScroll.VscrollbarPolicy = PolicyType.Automatic;
+            inputScroll.HscrollbarPolicy = PolicyType.Never;
+            
+            var inputView = new TextView();
+            
+            inputScroll.Add(inputView);
+            inputFrame.Add(inputScroll);
+            PackStart(inputFrame, true, true, AppSettings.Margin);
         }
     }
 }
