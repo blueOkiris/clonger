6e8f9e4476123232e3bd01797e4c2949e24ea001
Dylan Turner <dylantdmt@gmail.com>
2021-10-12 09:52:42 -0500

    Restructured main code to be cleaner and partitioned area for the three main sections

--- src/app.rs
+++ src/app.rs
@@ -11,16 +11,22 @@ use eframe::egui::{
 };
 use eframe::egui::menu;
 
+enum WindowState {
+    Document,
+    Dictionary,
+    Examples
+}
+
 pub struct ClongerWindow {
-    value : f32,
-    label : String
+    state : WindowState,
+    ipa_view_open : bool
 }
 
 impl Default for ClongerWindow {
     fn default() -> Self {
         Self {
-            value : 0.0,
-            label : String::from("Hello, world!")
+            state : WindowState::Document,
+            ipa_view_open : true
         }
     }
 }
@@ -31,58 +37,94 @@ impl App for ClongerWindow {
     }
 
     fn update(&mut self, ctx : &CtxRef, frame : &mut Frame<'_>) {
-        let Self { value, label } = self;
-
-        TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            menu::bar(ui, |ui| {
-                menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
-                });
-            });
-        });
-
-        SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(Layout::bottom_up(Align::Center), |ui| {
-                ui.add(
-                    Hyperlink::new("https://github.com/emilk/egui/").text("powered by egui"),
-                );
-            });
-        });
+        create_menu_bar(self, ctx, frame);
+        create_side_navigation(self, ctx, frame);
 
         CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
+            match self.state {
+                WindowState::Document => {
+                    ui.heading("Document");
 
-            ui.heading("egui template");
-            ui.hyperlink("https://github.com/emilk/egui_template");
-            ui.add(github_link_file!(
-                "https://github.com/emilk/egui_template/blob/master/",
-                "Source code."
-            ));
-            warn_if_debug_build(ui);
+                }, WindowState::Dictionary => {
+                    ui.heading("Dictionary");
+
+                }, WindowState::Examples => {
+                    ui.heading("Examples");
+
+                }
+            }
         });
 
-        if true {
-            Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
+        if self.ipa_view_open {
+            create_ipa_view(self, ctx, frame);
         }
     }
+}
+
+fn create_ipa_view(
+        me : &mut ClongerWindow, ctx : &CtxRef, frame : &mut Frame<'_>,) {
+    Window::new("IPA Typer").show(ctx, |ui| {
+        ui.label("Windows can be moved by dragging them.");
+        
+    });
+}
+
+fn create_side_navigation(
+        me : &mut ClongerWindow, ctx : &CtxRef, frame : &mut Frame<'_>,) {
+    SidePanel::right("View").show(ctx, |ui| {
+        ui.heading("View");
+
+        if ui.button("Toggle IPA").clicked() {
+            me.ipa_view_open = !me.ipa_view_open;
+        }
+        if ui.button("Document").clicked() {
+            me.state = WindowState::Document;
+        }
+        if ui.button("Dictionary").clicked() {
+            me.state = WindowState::Dictionary;
+        }
+        if ui.button("Examples").clicked() {
+            me.state = WindowState::Examples;
+        }
+    });
+}
+
+fn create_menu_bar(
+        me : &mut ClongerWindow, ctx : &CtxRef, frame : &mut Frame<'_>,) {
+    TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        menu::bar(ui, |ui| {
+            menu::menu(ui, "File", |ui| {
+                if ui.button("New").clicked() {
+                    
+                }
+                if ui.button("Save").clicked() {
+                    
+                }
+                if ui.button("Save As").clicked() {
+                    
+                }
+                if ui.button("Open").clicked() {
+                    
+                }
+                if ui.button("Exit").clicked() {
+                    frame.quit();
+                }
+            });
+            
+            menu::menu(ui, "View", |ui| {
+                if ui.button("Toggle IPA").clicked() {
+                    me.ipa_view_open = !me.ipa_view_open;
+                }
+                if ui.button("Document").clicked() {
+                    me.state = WindowState::Document;
+                }
+                if ui.button("Dictionary").clicked() {
+                    me.state = WindowState::Dictionary;
+                }
+                if ui.button("Examples").clicked() {
+                    me.state = WindowState::Examples;
+                }
+            });
+        });
+    });
 }
