de02cc2f9394dc8f53670265c0ecd5da6106af45
Dylan Turner <dylantdmt@gmail.com>
2021-10-12 10:32:12 -0500

    Changed default position of IPA view

--- src/ipaview.rs
+++ src/ipaview.rs
@@ -4,12 +4,14 @@
  */
 
 use crate::app::ClongerWindow;
-use eframe::egui::{ CtxRef, Window };
+use eframe::egui::{ CtxRef, Window, Pos2 };
 use eframe::epi::Frame;
 
 pub fn create_ipa_view(
         _me : &mut ClongerWindow, ctx : &CtxRef, _frame : &mut Frame<'_>,) {
-    Window::new("IPA Typer").show(ctx, |ui| {
+    Window::new("IPA Typer").default_pos(
+        Pos2::new(800000.0, 800000.0) // Put at bottom of screen
+    ).show(ctx, |ui| {
         ui.label("Windows can be moved by dragging them.");
         
     });
