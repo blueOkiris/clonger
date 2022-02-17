80e7e3c1c870c7048c08ae291074b76b591999e4
Dylan Turner <dylantdmt@gmail.com>
2022-02-17 16:34:35 -0600

    Adjusted goals of main window and how it will integrate with plugins

--- src/app.rs
+++ src/app.rs
@@ -5,15 +5,17 @@
 
 use gtk4::{
     Application, ApplicationWindow,
-    prelude::{ ApplicationExt, WidgetExt, ApplicationExtManual }
+    prelude::{
+        ApplicationExt, WidgetExt, ApplicationExtManual,
+    }
 };
 use std::env::set_var;
 
 const APP_ID: &'static str = "com.blueokiris.Clonger";
-const WIN_TITLE: &'static str = "Clonger";
-const WIN_DEF_WIDTH: i32 = 800;
-const WIN_DEF_HEIGHT: i32 = 600;
 const GSK_RENDERER: &'static str = "cairo";
+const WIN_MIN_WIDTH: i32 = 600;
+const WIN_MIN_HEIGHT: i32 = 800;
+const WIN_TITLE: &'static str = "Clonger";
 
 pub fn init_app() {
     set_var("GSK_RENDERER", GSK_RENDERER);
@@ -21,13 +23,23 @@ pub fn init_app() {
     app.connect_activate(|app| {
         let win = ApplicationWindow::builder()
             .application(app)
-            .default_width(WIN_DEF_WIDTH).default_height(WIN_DEF_HEIGHT)
             .title(WIN_TITLE)
+            .default_width(WIN_MIN_WIDTH).default_height(WIN_MIN_HEIGHT)
             .build();
-        
-        // TODO: Set up gui
+        win.set_size_request(WIN_MIN_WIDTH, WIN_MIN_HEIGHT);
 
+        setup_gui(&win);
+    
         win.show();
     });
     app.run();
+}
+
+fn setup_gui(win: &ApplicationWindow) {
+    // TODO: Add global data for file name and display it in a label
+    // TODO: Add content area where plugin pages will load
+    // TODO: Track changes & update file name based on if plugin changes (bool)
+    // TODO: Add keyboard shortcuts for creating new, opening, and saving files
+    // TODO: Create app pages from plugins and connect their events
+    // TODO: Create sub windows from plugins and connect their events
 }
